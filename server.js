de85343122ba30e14ab785f906a9fc211a397749
Sportsguy <mahoward2s@gmail.com>
2021-03-09 20:00:59 -0600

    renamed files, added code to connect routes

--- server.js
+++ server.js
@@ -4,8 +4,6 @@ const mongoose = require("mongoose");
 
 const PORT = process.env.PORT || 3000;
 
-//const db = require("./models");
-
 const app = express();
 
 app.use(logger("dev"));
@@ -15,10 +13,10 @@ app.use(express.json());
 
 app.use(express.static("public"));
 
-//-----------------------------------------------------------------------------
+mongoose.connect(process.env.MONGODB_URI || "mongodb://localhost/workout", { useNewUrlParser: true,
+useFindAndModify: false
+});
 
-/* mongoose.connect(process.env.MONGODB_URI || "mongodb://localhost/populatedb", { useNewUrlParser: true });
- */
 /* db.User.create({ name: "Ernest Hemingway" })
   .then(dbUser => {
     console.log(dbUser);
@@ -75,11 +73,11 @@ db.User.find({})
 })
 }); */
 
-//----------------------------------------------------------------------------
+
 
 //Routes
-//app.use(require("./routes/"));
-//app.use(require("./routes/"))
+app.use(require("./routes/apiRoutes"));
+app.use(require("./routes/htmlRoutes"))
 
 // Start the server
 app.listen(PORT, () => {
