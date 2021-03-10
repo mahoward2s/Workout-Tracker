55a7a9ef0c7f0f425533dca78f472b55972db3a3
Sportsguy <mahoward2s@gmail.com>
2021-03-09 20:05:56 -0600

    took route information out of server and put into api routes

--- server.js
+++ server.js
@@ -17,64 +17,6 @@ mongoose.connect(process.env.MONGODB_URI || "mongodb://localhost/workout", { use
 useFindAndModify: false
 });
 
-/* db.User.create({ name: "Ernest Hemingway" })
-  .then(dbUser => {
-    console.log(dbUser);
-  })
-  .catch(({ message }) => {
-    console.log(message);
-  });
-
-app.get("/notes", (req, res) => {
-  db.Note.find({})
-    .then(dbNote => {
-      res.json(dbNote);
-    })
-    .catch(err => {
-      res.json(err);
-    });
-});
-
-app.get("/user", (req, res) => {
-  db.User.find({})
-    .then(dbUser => {
-      res.json(dbUser);
-    })
-    .catch(err => {
-      res.json(err);
-    });
-});
-
-app.post("/submit", ({ body }, res) => {
-  db.Note.create(body)
-    .then(({ _id }) => db.User.findOneAndUpdate({}, { $push: { notes: _id } }, { new: true }))
-    .then(dbUser => {
-      res.json(dbUser);
-    })
-    .catch(err => {
-      res.json(err);
-    });
-});
-
-app.get("/populateduser", (req, res) => {
-  // TODO
-  // =====
-  // Write the query to grab the documents from the User collection,
-  // and populate them with any associated Notes.
-  // TIP: Check the models out to see how the Notes refers to the User
-
-db.User.find({})
-.populate("notes")
-.then(userWithNotes => {
-  res.json(usersWithNotes)
-})
-.catch (err => {
-  res.json(err)
-})
-}); */
-
-
-
 //Routes
 app.use(require("./routes/apiRoutes"));
 app.use(require("./routes/htmlRoutes"))
